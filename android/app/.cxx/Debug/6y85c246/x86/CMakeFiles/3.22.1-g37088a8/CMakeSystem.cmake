b34a7f7b4e1042b556a8ee36a5ee9e3d06778a04
Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-03-04 21:19:51 +0600

    Bangla language support added

--- android/app/.cxx/Debug/6y85c246/x86/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ android/app/.cxx/Debug/6y85c246/x86/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.11.0-14-generic")
+set(CMAKE_HOST_SYSTEM "Linux-6.11.0-17-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.11.0-14-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "6.11.0-17-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/ismail/Android/Sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake")
